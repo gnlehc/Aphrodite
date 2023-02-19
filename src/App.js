b6e1118b3e34e1a7a51eea28d6ed1e79b1d38d5c
gnlehc <Chelseang40@Gmail.com>
2023-02-19 14:14:42 +0700

    animation on products

--- src/App.js
+++ src/App.js
@@ -5,26 +5,22 @@ import Slider from "./components/slider/Slider";
 import Virtual from "./components/virtual/Virtual";
 import Products from "./components/products/Products";
 import Cart from "./components/cart/Cart";
-import { BrowserRouter as Router, Switch, Route, Link, redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  redirect,
+} from "react-router-dom";
 const App = () => {
   return (
-    <>
-      <div>
-        <link to="/">
-          <Header />
-          <Hero />
-          <Slider />
-          <Virtual />
-          <Products />
-        </link>
-      </div>
-      <div>
-        <link to="/cart">
-          <Header />
-          <Cart />
-        </link>
-      </div>
-    </>
+    <div>
+      <Header />
+      <Hero />
+      <Slider />
+      <Virtual />
+      <Products />
+    </div>
   );
 };
 
